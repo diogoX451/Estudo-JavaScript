5839f707911551971a6900fda9f5288030400512
diogoX451 <diogo.g07@icloud.com>
2022-07-20 20:38:18 -0300

    Atualização

---
+++ Array/array.js
@@ -0,0 +1,25 @@
+// Array é uma lista de coisas que podemos adicionar em um array
+// Todo array começa com indice 0
+// Para puxar um lista de coisas usa coxetes []
+const arrayCidade = ["São Paulo", "Rio de Janeiro", "Belo Horizonte"];
+console.log(arrayCidade);
+
+// Adicionar mais um lemento no array
+arrayCidade[3] = "Palmas";
+
+// Metodo mais recomendado para adicionar um elemento array
+arrayCidade.push("Manaus");
+console.log(arrayCidade);
+
+// Adicionar elemento do array se utilizando .lenght
+arrayCidade[arrayCidade.length] = "Rio Branco ";
+console.log(arrayCidade);
+
+//Adicionar um array no primeiro indice utiliza .unshift
+arrayCidade.unshift("Porto de Galinhas");
+console.log(arrayCidade);
+
+//Para remover um elemento do array  utiliza .pop
+//Pode salvar o que foi removido em uma variavel
+let remove = arrayCidade.pop();
+console.log(`O elemento que foi removido foi ${remove}`);
